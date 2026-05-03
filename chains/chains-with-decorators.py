f779be01b183399b6138b88956f5ae1b86b62769
jhonatanmizu <natanjesuss20@gmail.com>
2026-05-02 22:10:55 -0300

    chains with decorators

---
+++ chains/chains-with-decorators.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables.base import chain
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+
+@chain
+def square(input_dict: dict):
+    x = input_dict["number"]
+    print("numbers is ", x)
+    print("square is ", x * x)
+    return {"number": x * x}
+
+
+question_template = PromptTemplate(
+    template="Tell me about the square of {number}, with an explaination and other interesting facts",
+    input_variables=["number"],
+)
+model = ChatOpenAI(model="gpt-5-nano", temperature=0.5)
+
+chain = square | question_template | model
+result = chain.invoke({"number": 2})
+print(result.content)
